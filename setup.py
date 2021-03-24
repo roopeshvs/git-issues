1421923ee4b0d60bfa7a3fdc1687ace9dbf8b61a
roopeshvs <roopesh.vs@coda.global>
2021-03-24 21:04:37 +0530

    Added License

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 from setuptools import setup
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 requirements = [
     'click'
     'requests'
@@ -10,7 +13,8 @@ requirements = [
 setup(
     name='git-issues',
     version='0.1',
-    description='manage all your git issues at one place',
+    description='Manage all your git issues at one place',
+    long_description= long_description
     author='@roopeshvs',
     author_email='txtmeroopesh@gmail.com',
     url='https://github.com/roopeshvs/git-issues',
@@ -21,17 +25,13 @@ setup(
     license="BSD",
     zip_safe=False,
     classifiers=[
-        'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
         'Natural Language :: English',
-        "Programming Language :: Python :: 2",
-        'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
     ],
     entry_points='''
         [console_scripts]
